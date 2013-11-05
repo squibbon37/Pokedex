ef1d2243a68506f0d33ff3d9ea19753de101465c
squibbon37 <andrewlamb08@gmail.com>
2013-11-04 22:11:00 -0600

    adding pokemon to SQLite DB

--- pokemon_screenscrape/ScreenScrape.py
+++ pokemon_screenscrape/ScreenScrape.py
@@ -2,16 +2,36 @@ __author__ = 'Captain Vasoline'
 
 from bs4 import BeautifulSoup
 import urllib.request
+import sqlite3
 
 generation = ["I", "II", "III", "IV", "V", "VI"]
 
+db = sqlite3.connect('../pokedexDB.db')
+cur = db.cursor()
+
 
 def scrapescreen():
+    createtable()
     for version in generation:
         url = "http://pokemon.wikia.com/wiki/Category:Generation_" + version + "_Pok%C3%A9mon"
         content = urllib.request.urlopen(url).read()
         soup = BeautifulSoup(content)
         getpokemonforgeneration(soup)
+
+    cur.execute('SELECT * FROM Pokemon')
+    test = cur.fetchall()
+    print(test)
+
+    db.commit()
+    db.close()
+    print('You caught em all!')
+
+
+def createtable():
+    cur.execute('DROP TABLE IF EXISTS Pokemon')
+
+    cur.execute('''CREATE TABLE IF NOT EXISTS Pokemon(
+                    Number TEXT, Name TEXT, Type_1 TEXT, Type_2 TEXT)''')
 
 
 def getpokemonforgeneration(html):
@@ -25,6 +45,10 @@ def getpokemonforgeneration(html):
             type2 = pokemon.contents[5].text
             fullPokemon.append(type2)
 
-        print(fullPokemon)
+        if len(fullPokemon) < 4:
+            cur.execute('INSERT INTO Pokemon VALUES (?,?,?, NULL)', fullPokemon)
+
+        if len(fullPokemon) > 3:
+            cur.execute('INSERT INTO Pokemon VALUES (?,?,?,?)', fullPokemon)
 
 scrapescreen()
